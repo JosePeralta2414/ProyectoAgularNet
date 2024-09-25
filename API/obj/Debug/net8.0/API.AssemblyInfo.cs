ba6854787ba33a2e2c50c1bca0279f0d3cc5b202
JosePeralta2414 <aniquilator9000@gmail.com>
2024-09-25 07:58:18 -0600

    Parcial01 - First Entity

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3e6d5eb15d9be3c33c365fccd4d97cc23835f36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2eed9c453d5e627b2cb1daf1e245014fb3b276a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
