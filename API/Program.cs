00d793681193294beb53195353131153c174b80e
JosePeralta2414 <aniquilator9000@gmail.com>
2024-09-25 22:05:55 -0600

    Parcial01 - Add first ef migration

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,15 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddContext<DataContext>(opt =>
+builder.Services.AddDbContext<DataContext>(opt =>
 {
-    opt.UserSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e9ef604f4e9067c9c6b880dd6d96d8f57757395")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31d584100d807e577884042b1dd509ad2e0b8c6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
