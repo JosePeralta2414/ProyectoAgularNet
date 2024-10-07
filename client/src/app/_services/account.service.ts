35e78545cd8182ee947a2f63051b2c82a977b2d8
JosePeralta2414 <aniquilator9000@gmail.com>
2024-10-06 23:47:37 -0600

    Parcial01 - Add user registration

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { User } from '../_models/user';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -17,6 +18,18 @@ export class AccountService {
           localStorage.setItem("user", JSON.stringify(user));
           this.currentUser.set(user);
         }
+      })
+    );
+  }
+  
+  register(model: any): Observable<User | void> {
+    return this.http.post<User>(this.baseUrl + "account/register", model).pipe(
+      map((user) => {
+        if (user) {
+          localStorage.setItem("user", JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+        return user;
       })
     );
   }
