2363fee218e1fc3829f058b7957a044153bd7f65
JosePeralta2414 <aniquilator9000@gmail.com>
2024-10-26 19:41:47 -0600

    Parcial02 - Add REPOSITORY PATTERN

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -14,6 +14,8 @@ public static class ApplicationServiceExtensions
         });
         services.AddCors();
         services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IUserRepository, UserRepository>();
+
         return services;
     }
 }
