eea0fe30a1838b55a78bee1aad52fb36853254a3
JosePeralta2414 <aniquilator9000@gmail.com>
2024-11-20 20:49:31 -0600

    Parcial01-Parcial01

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a623528d108ed5f6b5c791ed7d844eda9a6cdee9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a986827c8792320e7165b1b659ffc709c02e6303")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
