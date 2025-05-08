75008659acfd82ce64aaf2e3c6387bf7f60a5358
JosePeralta2414 <aniquilator9000@gmail.com>
2025-05-08 15:06:34 -0600

    Parcial05 - Add ActionFilter

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6e51a4a34b974cf7298d9007da52191d0652cd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+651c9b82329febc673749e4ab71d418106789db1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
