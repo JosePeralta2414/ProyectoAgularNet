5f3ee760225d40f647b3a37115e311e629bc662d
JosePeralta2414 <aniquilator9000@gmail.com>
2025-05-08 16:57:56 -0600

    Parcial05 - Add Get Messages for User

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5606e923776d3f19ec213dd785852715c99a1499")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84355d2e51d6c82af49c562dbcd8f1f2ff573c6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
