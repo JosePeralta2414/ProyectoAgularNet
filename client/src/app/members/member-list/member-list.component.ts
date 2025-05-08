86b9271d49cf93d8d501affda3a84a0aefdc97b3
JosePeralta2414 <aniquilator9000@gmail.com>
2025-05-08 14:51:17 -0600

    Parcial05 - Clean up member service

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { MemberCardComponent } from '../member-card/member-card.component';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { AccountService } from '../../_services/account.service';
+import { UserParams } from '../../_models/userParams';
 @Component({
   selector: 'app-member-list',
   standalone: true,
@@ -9,12 +11,11 @@ import { PaginationModule } from 'ngx-bootstrap/pagination';
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
-
 export class MemberListComponent implements OnInit {
+  private accountService = inject(AccountService);
   membersService = inject(MembersService);
-  pageNumber = 1;
-  pageSize = 5;
-  
+  userParams = new UserParams(this.accountService.currentUser());
+
   ngOnInit(): void {
     if (!this.membersService.paginatedResult()) {
       this.loadMembers();
@@ -22,12 +23,12 @@ export class MemberListComponent implements OnInit {
   }
 
   loadMembers() {
-    this.membersService.getMembers(this.pageNumber, this.pageSize);
+    this.membersService.getMembers(this.userParams);
   }
 
   pageChanged(event: any) {
-    if (this.pageNumber !== event.page) {
-      this.pageNumber = event.page;
+    if (this.userParams.pageNumber !== event.page) {
+      this.userParams.pageNumber = event.page;
       this.loadMembers();
     }
   }
