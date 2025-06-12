2d4c5924c915191b2a33d87b4ee248d0750cf0ff
JosePeralta2414 <aniquilator9000@gmail.com>
2025-06-11 20:35:00 -0600

    Parcial06 - Add policy based authorization

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d60201f04b9bbfb63d7e5593c786bd1aa65329be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b305493af0c6893df9582d562d8e1106f33e289")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
