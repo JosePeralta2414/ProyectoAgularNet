cf0ccf168ba91edf838eb02b3bc1af97b481bb74
JosePeralta2414 <aniquilator9000@gmail.com>
2025-06-11 20:44:24 -0600

    Parcial06 - Add Admin component and guard

---
+++ client/src/app/admin/admin-panel/admin-panel.componet.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';More actions
+
+@Component({
+  selector: 'app-admin-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-panel.component.html',
+  styleUrl: './admin-panel.component.css'
+})
+export class AdminPanelComponent {
+
+}
