9ae62c73cec54c2627cf227b64638331fc424321
JosePeralta2414 <aniquilator9000@gmail.com>
2025-06-11 21:47:20 -0600

    Parcial06 - Add sending messages using SugnalR Hub

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -16,11 +16,9 @@ export class MemberMessagesComponent {
   messageContent = "";
 
   sendMessage() {
-    this.messagesService.sendMessage(this.username(), this.messageContent).subscribe({
-      next: message => {
-        this.messageForm?.reset();
-      }
-    });
-    
+    this.messagesService.sendMessage(this.username(), this.messageContent).then(() => {
+      this.messageForm?.reset();
+    })
+  
   }
 }
